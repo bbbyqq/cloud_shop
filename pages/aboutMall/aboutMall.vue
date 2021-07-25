47b71d55d0c866972a25726099cbe3a5a31824fd
bbbyqq <59790931+bbbyqq@users.noreply.github.com>
2021-07-25 19:17:56 +0800

    Update aboutMall.vue

--- pages/aboutMall/aboutMall.vue
+++ pages/aboutMall/aboutMall.vue
@@ -3,7 +3,6 @@
 		<view style="text-indent: 2em;">云商城项目于2021年5月26日完成，该项目为纯前端项目，未调用后端接口，若使用者在使用过程中遇到了bug</view>
 		<view style="margin-top: 40rpx;">请联系:</view>
 		<view>QQ：2287532812</view>
-		<view>手机：18729809812</view>
 	</view>
 </template>
 
